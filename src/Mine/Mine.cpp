af497ed6e0d41274ff523b887578eaa4e3c2dc38
hemingguo <sosljsos@qq.com>
2023-12-03 15:51:53 +0800

    mine

--- src/Mine/Mine.cpp
+++ src/Mine/Mine.cpp
@@ -3,3 +3,68 @@
 //
 
 #include "Mine.h"
+
+//Mine-----------
+string Mine::getName()
+{
+    return this->name;
+}
+
+int Mine::getValue()
+{
+    return this->value;
+}
+
+Mine::Mine() : name("unkonwn"), value(0)
+{
+
+}
+
+Mine::~Mine() {}
+
+//----------------Mine
+
+
+
+//Iron---------------
+Iron::Iron()
+{
+    name = "iron";
+    value = 5;
+    divisible = false;
+}
+
+Iron::~Iron() {}
+
+void Iron::paintIron() {}
+//---------------Iron
+
+
+
+//Copper--------------
+Copper::Copper()
+{
+    name = "copper";
+    value = 10;
+    divisible = true;
+}
+
+Copper::~Copper() {}
+
+void Copper::paintCopper() {}
+//--------------Copper
+
+
+
+//HalfCopper-------------
+HalfCopper::HalfCopper()
+{
+    name = "halfcopper";
+    value = 5;
+    divisible = false;
+}
+
+HalfCopper::~HalfCopper() {}
+
+void HalfCopper::paintHalfCopper() {}
+//-------------HalfCopper
--- src/Mine/Mine.h
+++ src/Mine/Mine.h
@@ -5,10 +5,60 @@
 #ifndef SHAPEZ_MINE_H
 #define SHAPEZ_MINE_H
 
+#include <string>
+
+using namespace std;
 
 class Mine
 {
+protected:
+    string name; //矿物名称
+    int value;  //矿物价值
+    Mine();  //构造函数
+    ~Mine(); //析构函数
+public:
+    bool divisible;  //矿物是否可以被切割
+    string getName();  //返回矿物名称
+    int getValue();   //返回矿物价值
 
+
+};
+
+
+class Iron : public Mine
+{
+protected:
+    Iron();
+
+    ~Iron();
+
+public:
+    void paintIron();
+};
+
+
+class Copper : public Mine
+{
+
+
+protected:
+    Copper();
+
+    ~Copper();
+
+public:
+    void paintCopper();
+};
+
+
+class HalfCopper : public Copper
+{
+    HalfCopper();
+
+    ~HalfCopper();
+
+public:
+    void paintHalfCopper();
 };
 
 
